f4ea7a3b1902dbac6a4f8c67cd3ca3b1ebda0bdc
Tomasz Kuczynski <tkuczyn@gmail.com>
2016-07-30 22:14:58 -0400

    Feature Change: Generalized the keyboard controller

--- js/keyboardController.js
+++ js/keyboardController.js
@@ -5,20 +5,8 @@ var app = app || {};
 app.KeyboardController = function(){
 
 	var KeyboardController = function(){
-        this.KEYBOARD = {
-			"KEY_LEFT_A": 65,
-            "KEY_LEFT_ARROW": 37,
-			"KEY_UP_W": 87,
-            "KEY_UP_ARROW": 38,
-			"KEY_RIGHT_D": 68,
-            "KEY_RIGHT_ARROW": 39,
-			"KEY_DOWN_S": 83,
-            "KEY_DOWN_ARROW": 40,
-			"KEY_SPACE": 32,
-			"KEY_R": 82
-		};
-
-        this.keydown = [];
+        this.keyActions = [];
+				this.keydown = [];
     };
 
     var p = KeyboardController.prototype;
@@ -27,32 +15,31 @@ app.KeyboardController = function(){
         var _this = this;
 
         window.addEventListener("keydown", function(e){
-            _this.keydown[e.keyCode] = true;
+            _this.keydown[e.key] = true;
         });
 
         window.addEventListener("keyup", function(e){
-            _this.keydown[e.keyCode] = false;
+            _this.keydown[e.key] = false;
         });
-    }
+    };
+
+		p.assignKeyAction = function (keys, action)
+		{
+			for(var i = 0; i < keys.length; i++)
+			{
+				this.keyActions[keys[i]] = action;
+			}
+		};
 
     p.update = function(entity){
-        if(this.keydown[this.KEYBOARD.KEY_LEFT_A] ||
-            this.keydown[this.KEYBOARD.KEY_LEFT_ARROW]){
-            entity.applyWorldForces([vec2.fromValues(-2,0)]);
-		}
-		if(this.keydown[this.KEYBOARD.KEY_RIGHT_D] ||
-            this.keydown[this.KEYBOARD.KEY_RIGHT_ARROW]){
-            entity.applyWorldForces([vec2.fromValues(2,0)]);
-		}
-		if(this.keydown[this.KEYBOARD.KEY_UP_W] ||
-            this.keydown[this.KEYBOARD.KEY_UP_ARROW]){
-            entity.applyWorldForces([vec2.fromValues(0,-2)]);
-		}
-		if(this.keydown[this.KEYBOARD.KEY_DOWN_S] ||
-            this.keydown[this.KEYBOARD.KEY_DOWN_S]){
-            entity.applyWorldForces([vec2.fromValues(0,2)]);
-		}
-    }
+			for(var key in this.keyActions)
+			{
+				if(this.keydown[key])
+				{
+					this.keyActions[key](entity);
+				}
+			}
+    };
 
     return KeyboardController;
 
