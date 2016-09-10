2e74736df920a666b548b36f0c93f4eec27a188d
Alan Leeson <alanleeson@Alans-Air.fios-router.home>
2016-09-10 18:33:19 -0400

    #Changed number of spawning objects

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ app.Main = {
 		this.world = new app.World(this.loadedForces);
 		var bounds = this.bounds;
 		this.world.setUpdateFunction(function(){
-			while(this.numEntities() < 50){
+			while(this.numEntities() < 8){
 				var entity =	new app.Entity(
 					bounds['width'] * Math.random(), 0,
 					Math.random() * 10 + 5,app.draw.randomRGBA(200,0,0.5), 20, "moveable");
